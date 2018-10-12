c290a75922f0b776163964f39f7a423dd4e8f442
Lawrence Custodio <lcustodio@fb.com>
2018-10-11 17:59:39 -0700

    Initial setup and commit

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class App extends React.Component {
+    render(): React.Node {
+        return (
+            <div className="container" data-testid="toggle-container">
+                Hello Hello
+          </div>
+        );
+    }
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+import 'milligram/milligram.min.css';
+
+import App from './components/App';
+
+render(<App />, document.getElementById('app'));
